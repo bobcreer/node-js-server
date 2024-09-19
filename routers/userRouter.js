3bd896d1cde84532154687c1692234b64b7c4bbd
Bob <bobcreer@gmail.com>
2024-09-19 09:48:17 +0800

    first commit

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
-const { login, register } = require('../controllers/userController')
+const { login, register, currentUser } = require('../controllers/userController')
+const auth = require('../middlewares/auth')
 
 const userRouter = Router()
 
 userRouter.post('/login', login)
 userRouter.post('/register', register)
+userRouter.post('/currentUser', auth, currentUser)
 
 module.exports = userRouter
